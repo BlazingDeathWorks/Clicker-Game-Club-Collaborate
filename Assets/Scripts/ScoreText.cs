fb09a1eb71c7b22ab8332fa276b98b496045c73d
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2021-12-10 17:07:17 -0800

    Delete start, update methods
    As per @BlazingDeathWorks

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -12,16 +12,5 @@ public class ScoreText : MonoBehaviour
     {
         counter += 1;
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
