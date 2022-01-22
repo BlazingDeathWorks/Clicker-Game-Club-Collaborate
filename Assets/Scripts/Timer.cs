0ae27b74d4a6a880ff4f799e21be6afefec8f3c4
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2022-01-21 16:59:43 -0800

    Patch #8

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Timer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        [SerializeField] Text timerText;
+        float timeLeft = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        timerText.text = $"Time Left: {(int)Math.Round(timeLeft, 0)}";
     }
 }
