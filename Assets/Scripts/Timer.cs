e624ccdead4bc499afd48655ee744a3fd8953d6d
BlazingDeathWorks <62969110+BlazingDeathWorks@users.noreply.github.com>
2022-01-21 17:20:25 -0800

    Updated Timer.cs (#21)

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        [SerializeField] Text timerText;
-        float timeLeft = 60;
-    }
+    [SerializeField] Text timerText;
+    float timeLeft = 60;
 
     // Update is called once per frame
     void Update()
